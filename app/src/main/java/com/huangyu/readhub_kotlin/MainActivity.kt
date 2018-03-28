fa9cc12b482616a35fdc72c8a68cfaf7c386d38b
huangyu0522 <384539643@qq.com>
2018-03-28 10:00:46 +0800

    init

---
+++ app/src/main/java/com/huangyu/readhub_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.huangyu.readhub_kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
